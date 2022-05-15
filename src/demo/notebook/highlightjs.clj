f0dbbcc3ab89b1f1438d1215a69f87f9a8944fb8
awb99 <andreas.wolfgang.bauer@gmail.com>
2022-05-15 07:03:06 +0200

    ext config

--- src/demo/notebook/highlightjs.clj
+++ src/demo/notebook/highlightjs.clj
@@ -2,6 +2,6 @@
 
 ^:R
 [:div
- [:p "this snippet demonstrates to usehighlight.js"]
+ [:p "this snippet demonstrates to use highlight.js"]
  [:p "eval button click will just print the code to browser console."]
- [:p/highlightjs "(+ 8 8)\n(def add [a b] \n   (+ a b))\n(add)"]]
+ ['user/highlightjs "(+ 8 8)\n(def add [a b] \n   (+ a b))\n(add)"]]
