d47b5a8bc3ba2284b0070b557d785388d211cef0
awb99 <awb99@gmail.com>
2024-04-17 14:36:19 -0500

    css tuning

--- resources/demo/notebook/highlightjs.clj
+++ resources/demo/notebook/highlightjs.clj
@@ -5,3 +5,10 @@
  [:p "this snippet demonstrates to use highlight.js"]
  ['ui.highlightjs/highlightjs "(+ 8 8)\n(def add [a b] \n   (+ a b))\n(add)"]
  [:p "live goes on!"]]
+
+
+^:R
+[:div.bg-blue-500 ; test how background is on different color
+ [:p "look at --> "]
+ ['ui.highlightjs/highlightjs "(->> (map inc (range 10))\n   (take 3))"]
+ [:p "live goes on!"]]
--- src/deps.cljs
+++ src/deps.cljs
@@ -1,9 +1,5 @@
 {:npm-deps
- {; in projects that use notebook-ui
-  ; shadow-cljs creates package.json 
-  ; based on this dependencies 
-
-  "highlight.js" "^11.0.1" ; code-coloring ; also used by 10x
+ {"highlight.js" "^11.0.1" ; code-coloring ; also used by 10x
   ;"highlightjs-line-numbers.js" "^2.8.0"
   ; see: https://github.com/wcoder/highlightjs-line-numbers.js/issues/77s
 
